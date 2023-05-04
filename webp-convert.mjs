0c7c09097169583104d3144375eb5efec26d24c2
Marina Khadieva <utabond89@gmail.com>
2023-05-04 16:14:49 +0300

    Added footer

--- webp-convert.mjs
+++ webp-convert.mjs
@@ -1,8 +1,22 @@
 import imagemin from "imagemin";
 import imageminWebp from "imagemin-webp";
 
-await imagemin(['src/img/*.{jpg,png}'], {
-	destination: 'src/img',
+await imagemin(['src/img/career/*.{jpg,png}'], {
+	destination: 'src/img/career',
+	plugins: [
+		imageminWebp({ quality: 70 }),
+	]
+});
+
+await imagemin(['src/img/about/*.{jpg,png}'], {
+	destination: 'src/img/about',
+	plugins: [
+		imageminWebp({ quality: 70 }),
+	]
+});
+
+await imagemin(['src/img/hero/*.{jpg,png}'], {
+	destination: 'src/img/hero',
 	plugins: [
 		imageminWebp({ quality: 70 }),
 	]
